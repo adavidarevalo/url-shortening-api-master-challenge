98979cd67160992761b06e0815e56dfeece969bc
David Arevalo <davidarevaloc20@gmail.com>
2021-05-31 21:04:57 -0500

    part of logic

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,35 @@
 // header
-const menuButton = document.querySelector(".fa-bars")
+const menuButton:Element = document.querySelector(".fa-bars")
 const navMenu = document.querySelector("#navMenu")
 let displayNone: string = "displayNone"
 menuButton.addEventListener("click", ()=>{
     navMenu.classList.toggle(`${displayNone}`)
 })
+// Api 
+const mainInputUrl: any = document.querySelector("#mainInputUrl")
+const mainButtonUrl: Element = document.querySelector("#mainButtonUrl")
+const Api: string= "https://api.shrtco.de/v2/shorten?url="
+
+mainButtonUrl.addEventListener("click", ()=>{
+  if(mainInputUrl.value == ""){
+    console.log("Ingrese un dato")
+  } else {
+    checkUrl(mainInputUrl.value)
+  }
+})
+
+function checkUrl(url:string | any) {
+  if(url.includes(".")){
+    ApiFetch("https://api.shrtco.de/v2/shorten?url=", mainInputUrl.value)
+  } else {
+    console.log("This Url is not Valid")
+  }
+}
+
+function ApiFetch(api: string, complement: string|any) {
+  fetch(`${api}+${complement}`)
+    .then(response => response.json())
+    console.log(response)
+    .catch(error => console.log(error.message)))
+}
+
