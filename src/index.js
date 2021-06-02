95bfd7eac2b95fb890c9413f22f42ef3e8f72204
David Arevalo <davidarevaloc20@gmail.com>
2021-06-02 15:23:00 -0500

    webpack

--- src/index.js
+++ src/index.js
@@ -11,35 +11,50 @@ var mainButtonUrl = document.querySelector("#mainButtonUrl");
 var Api = "https://api.shrtco.de/v2/shorten?url=";
 var mainShortenResult = document.querySelector("#mainShortenResult");
 var buttonLoader = document.querySelector("#buttonLoader");
+var mainMessageError = document.querySelector("#mainMessageError");
 mainButtonUrl.addEventListener("click", function () {
     if (mainInputUrl.value == "") {
-        console.log("Ingrese un dato");
+        errorMessage();
     }
     else {
         checkUrl(mainInputUrl.value);
     }
 });
 function checkUrl(url) {
-    if (url.includes(".")) {
+    if (url.includes(" ")) {
+        errorMessage();
+        console.log(mainInputUrl.value == "");
+    }
+    else if (url.includes(".")) {
         var makeApi = "" + Api + url;
         urlFetch(makeApi);
         buttonLoader.innerHTML = "\n    <img class=\"button-loader\" src=\"https://acegif.com/wp-content/uploads/loading-11.gif\" alt=\"loader git\">\n    ";
-    }
-    else {
-        console.log("This Url is not Valid");
+        //delete error message
+        mainInputUrl.style = "border: none;";
+        mainMessageError.innerHTML = "";
     }
 }
 function urlFetch(url) {
     fetch(url)
         .then(function (response) { return response.json(); })
-        .then(function (data) { return templateResult(data); })["catch"](function (err) { return console.log("Error"); });
+        .then(function (data) { return templateResult(data.result.short_link); })["catch"](function (err) { return apiError(err); });
 }
 function templateResult(data) {
     console.log(data);
     var infoDiv = document.createElement("div");
-    var infoP = document.createElement("p");
-    infoP.innerText = "" + data;
-    infoDiv.appendChild(infoP);
+    infoDiv.innerHTML = "\n  <div class=\"main-url_container\">\n    <div class=\"origin-Url\">\n      <p class=\"displayNone\">" + mainInputUrl.value + "</p>\n    </div>\n    <div class=\"url-container_result\">\n      <a href=\"https://" + data + "\" target=\"_blank\" class=\"main-url\">" + data + "</a>\n      <button id=\"urlCopy\">Copy</button>\n    </div>\n  </div>\n  ";
+    mainShortenResult.append(infoDiv);
+    buttonLoader.innerHTML = "";
+    mainInputUrl.value = "";
+}
+function apiError(data) {
+    console.log(data);
+    var infoDiv = document.createElement("div");
+    infoDiv.innerHTML = "\n  <p>That Url is not accept. Try again.</p>\n  ";
     mainShortenResult.appendChild(infoDiv);
     buttonLoader.innerHTML = "";
+}
+function errorMessage() {
+    mainInputUrl.style = "border: 1px solid red;";
+    mainMessageError.innerHTML = "\n    <p>You need to review your url. Try again</p>\n    ";
 }
--- src/index.ts
+++ src/index.ts
@@ -11,40 +11,71 @@ const mainButtonUrl: Element = document.querySelector("#mainButtonUrl")
 const Api: string= "https://api.shrtco.de/v2/shorten?url="
 const mainShortenResult: Element = document.querySelector("#mainShortenResult")
 let buttonLoader: Element = document.querySelector("#buttonLoader")
+let mainMessageError = document.querySelector("#mainMessageError")
 
 mainButtonUrl.addEventListener("click", ()=>{
   if(mainInputUrl.value == ""){
-    console.log("Ingrese un dato")
+    errorMessage()
   } else {
     checkUrl(mainInputUrl.value)
   }
 })
 
 function checkUrl(url:string | any) {
-  if(url.includes(".")){
+  if(url.includes(" ")){
+    errorMessage();
+    console.log(mainInputUrl.value == "")
+  } else if(url.includes(".")){
     const makeApi: string = `${Api}${url}`
     urlFetch(makeApi);
     buttonLoader.innerHTML=`
     <img class="button-loader" src="https://acegif.com/wp-content/uploads/loading-11.gif" alt="loader git">
     `
-  } else {
-    console.log("This Url is not Valid")
+    //delete error message
+    mainInputUrl.style=`border: none;`
+    mainMessageError.innerHTML=``
   }
 }
 
 function urlFetch(url:string) {
   fetch(url)
     .then(response => response.json())
-    .then(data => templateResult(data))
-    .catch(err => console.log("Error"))
+    .then(data => templateResult(data.result.short_link))
+    .catch(err => apiError(err))
 }
 
 function templateResult(data: object) {
   console.log(data)
   let infoDiv=document.createElement("div")
-  let infoP= document.createElement("p")
-  infoP.innerText= `${data}`
-  infoDiv.appendChild(infoP)
+  infoDiv.innerHTML= `
+  <div class="main-url_container">
+    <div class="origin-Url">
+      <p class="displayNone">${mainInputUrl.value}</p>
+    </div>
+    <div class="url-container_result">
+      <a href="https://${data}" target="_blank" class="main-url">${data}</a>
+      <button id="urlCopy">Copy</button>
+    </div>
+  </div>
+  `
+  mainShortenResult.append(infoDiv)
+  buttonLoader.innerHTML=``
+  mainInputUrl.value=""
+}
+function apiError(data) {
+  console.log(data)
+  let infoDiv=document.createElement("div")
+  infoDiv.innerHTML= `
+  <p>That Url is not accept. Try again.</p>
+  `
   mainShortenResult.appendChild(infoDiv)
   buttonLoader.innerHTML=``
+}
+
+
+function errorMessage() {
+  mainInputUrl.style=`border: 1px solid red;`
+    mainMessageError.innerHTML=`
+    <p>You need to review your url. Try again</p>
+    `
 }
